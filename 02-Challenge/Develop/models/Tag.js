f30a0563a982c41bac7197b8f32be3fc943a702a
Cyberdev <turnerjeniah8@gmail.com>
2022-12-15 14:08:38 -0800

    Finished editing the datatypes models, made sure there was validation for the decimal points

---
+++ 02-Challenge/Develop/models/Tag.js
@@ -0,0 +1,45 @@
+const { Model, DataTypes } = require('sequelize');
+
+const sequelize = require('../config/connection.js');
+
+class Tag extends Model {}
+
+Tag.init(
+  {
+    id: {
+    type: DataTypes.INTEGER, 
+    allowNull: false,
+    primaryKey: true, 
+    autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
+    product_id: {
+      type: DataTypes.INTEGER,
+      //references the product model id here
+      references: {
+        model: 'product',
+        key: 'id',
+     },
+    },
+
+    tag_id: {
+      type: DataType.INTEGER,
+      //references the tag model id here
+      references: {
+        model: 'tag',
+        key: 'id',
+     },
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'tag',
+  }
+);
+
+module.exports = Tag;
