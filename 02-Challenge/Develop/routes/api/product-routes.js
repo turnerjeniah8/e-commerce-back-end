3bb6b7b4d528aa8fdc09bef2982af6f1899dd7fd
Cyberdev <turnerjeniah8@gmail.com>
2022-12-23 01:36:11 -0800

    Finished adding comments to the category routes. Finished all routes pertaining to the products. As well as comments. Next step is to work on the tag routes.

--- 02-Challenge/Develop/routes/api/product-routes.js
+++ 02-Challenge/Develop/routes/api/product-routes.js
@@ -4,15 +4,35 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  //we want to include all products within the model category. We also want to include the tage associated with the products
+  //then if it works the response would be the server status of 200, anything else will recieve an error message
+  try{ 
+    const productData = await Product.findAll({
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags_on_product' }]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  //we want to find the item by including all products within the category model. 
+  
+try{ 
+  const productData = await Product.findByPk(req.params.id, {
+    include: [{model: Category}, {model: Tag, through: ProductTag, as: 'tags_on_product'}]
+  });
+  res.status(200).json(productData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 // create new product
@@ -89,8 +109,25 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  //we first want the product to delete by matching it with its ID. 
+  //if it works then the item will delete, if it doesnt work then the user will be presented with the error message
+  
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'No product found with this id, try again.'});
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
